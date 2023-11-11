0390eb4f8be5d2569970fffc1a0e20556f1a2af6
rcsen <rohansen856@gmail.com>
2023-11-12 03:46:03 +0530

    modified:   .eslintrc.json 	modified:   public/sandip.jpeg 	new file:   src/app/(profiles)/profile/layout.tsx 	new file:   src/app/(profiles)/profile/page.tsx 	new file:   src/components/profile/home-1.tsx 	modified:   src/components/side-nav.tsx 	new file:   src/hooks/use-intersection-observer.ts 	new file:   src/hooks/use-local-storage.ts 	new file:   src/hooks/use-media-query.ts 	new file:   src/hooks/use-scroll.ts 	modified:   src/styles/globals.css

---
+++ src/app/(profiles)/profile/page.tsx
@@ -0,0 +1,7 @@
+import Home from "@/components/profile/home-1"
+
+export default function ProfilePage() {
+  return (
+    <Home />
+  )
+}
--- src/components/side-nav.tsx
+++ src/components/side-nav.tsx
@@ -2,14 +2,9 @@
 
 import { Separator } from "@/components/ui/separator"
 
-const container = {
-    hidden: { x: -150 },
-    show: { x: 0 },
-}
-
 export function SideNav({ className }: React.HTMLAttributes<HTMLElement>) {
     return (
-        <h1 className="m-auto ml-[-150px] hidden h-full  w-[150px] translate-x-[150px] font-heading text-7xl font-bold transition-transform delay-700 ease-out md:flex">
+        <h1 className="m-auto hidden h-full w-[150px] font-heading text-7xl font-bold md:flex">
             <span className="m-auto max-w-full rotate-180">
                 <p className="rotate-90">D104</p>
             </span>
