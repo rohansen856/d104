27090c69fa4b942423d2e7a73166baa42a03ae46
rcsen <rohansen856@gmail.com>
2023-11-20 23:12:45 +0530

    modified:   src/lib/auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import NextAuth, { NextAuthConfig } from "next-auth"
-import GitHub from "next-auth/providers/gitHub"
+import GitHub from "next-auth/providers/github"
 
 import { db } from "@/lib/db"
 
